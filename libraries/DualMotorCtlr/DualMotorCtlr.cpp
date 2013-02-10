0bf5bcf4e66a20a025f24e7f8d18c8ee8e0a92dd
botguy <botguy@gmail.com>
2013-02-10 21:01:01 +0000

    DualMotorControl library

---
+++ libraries/DualMotorCtlr/DualMotorCtlr.cpp
@@ -0,0 +1,38 @@
+#include "Arduino.h"
+#include "DualMotorCtlr.h"
+
+void DualMotorInit(void)
+{
+  // initialize outputs to 0
+  DualMotorControl(0,0);
+
+  // enable outputs
+  pinMode(a1    , OUTPUT);
+  pinMode(a2    , OUTPUT);
+  pinMode(aPWM  , OUTPUT);
+  
+  pinMode(b1    , OUTPUT);
+  pinMode(b2    , OUTPUT);
+  pinMode(bPWM  , OUTPUT);
+}
+
+//
+void DualMotorControl(int16_t sped, int16_t turn)
+{
+  int16_t right, left;
+  
+  right = sped + turn;
+  left  = sped - turn;
+  
+  digitalWrite(a1, right >  0);
+  digitalWrite(a2, right <= 0);  
+  right = abs(right);
+  right = min(right, 255);
+  analogWrite(aPWM, right); 
+  
+  digitalWrite(b1, left >  0);
+  digitalWrite(b2, left <= 0);  
+  left = abs(left);
+  left = min(left, 255);
+  analogWrite(bPWM, left); 
+}
