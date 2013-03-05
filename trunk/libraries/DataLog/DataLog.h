abfbe0d5cda300540bb44246b9e51dbf41643989
botguy@gmail.com <botguy@gmail.com@0d006a46-d63c-e3c8-9189-067751c37eed>
2013-03-05 05:13:15 +0000

    Refine DataLog sizing parameters

--- trunk/libraries/DataLog/DataLog.h
+++ trunk/libraries/DataLog/DataLog.h
@@ -1,7 +1,13 @@
 #ifndef DataLog_h
 #define DataLog_h
 
-#define NUM_DATA_POINTS	100
+// configuration parameters
+#define RECORD_TIME_MS			(250ull)	// record for 250ms
+#define NUM_RECORDED_SIGNALS	(4) 	// record 4 variables
+#define SAMPLING_RATE			(500) 	// 500Hz sampling rate
+
+// computed parameters
+#define NUM_DATA_POINTS			(NUM_RECORDED_SIGNALS * SAMPLING_RATE * RECORD_TIME_MS / 1000) 	// 0.25 second of 4 data points at 500Hz
 
 // Data Logging
 void startDataLogging(void);
