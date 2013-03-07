96b187bf58ce667a6a6f2a048c69b64464749ac2
kshosford <kshosford@gmail.com>
2013-03-07 05:09:04 +0000

    1.  Added RCcontrol 2.  Swapped motors and changed direction of motors 3.  Stopped data logging to print data

--- libraries/DualMotorCtlr/DualMotorCtlr.h
+++ libraries/DualMotorCtlr/DualMotorCtlr.h
@@ -6,13 +6,13 @@
 #include "Arduino.h"
 
 // Use timer1 for PWM (pins 9 and 10)
-#define   a1    8
-#define   a2    11
-#define   aPWM  9
+#define   b1    11
+#define   b2    8
+#define   bPWM  9
 
-#define   b1    12
-#define   b2    13
-#define   bPWM  10
+#define   a1    13
+#define   a2    12
+#define   aPWM  10
 
 void DualMotorInit(void);
 
---
+++ libraries/RcControl/RcControl.cpp
@@ -0,0 +1,49 @@
+#include "Arduino.h"
+#include <RcControl.h>
+#include <ControlLoopHandler.h>
+#include <DataLog.h>
+
+void RcControl( Stream* stream )
+{
+	if( stream->available() ) // input data from USB or bluetooth serial
+	{
+		char recieveChar = stream->read();
+		switch( recieveChar )
+		{
+			case EMERGENCY_STOP: // Emergency stop
+				EmergencyStop();
+				break;
+				
+			case PRINT_DATA_LOG: // print data log
+				printDataLog( stream );
+				break;
+				
+			case FORWARD:
+				setSped(SPEED_MAGNITUDE);
+				break;
+				
+			case STOP:
+				setSped( 0 );
+				setTurn( 0 );
+				break;
+				
+			case BACK:
+				setSped(-SPEED_MAGNITUDE);
+				break;
+				
+			case LEFT:
+				setTurn( TURN_MAGNITUDE );
+				break;
+				
+			case RIGHT:
+				setTurn(-TURN_MAGNITUDE );
+				break;
+			
+			case UPLOAD_TUNING:
+				break;
+				
+			default:
+				break;
+		}
+	}
+}
