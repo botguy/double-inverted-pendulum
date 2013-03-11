aec6944faf83c18c847c8c9f1a801947dd3ed3cf
kshosford@gmail.com <kshosford@gmail.com@0d006a46-d63c-e3c8-9189-067751c37eed>
2013-03-11 00:29:58 +0000

    1. Setup timer for 10-bit Fast PWM 2. Changed center point for single inverted 3. Added SingleInvertedDZ1

---
+++ trunk/libraries/SingleInvertedDZ1/SingleInvertedDZ1.h
@@ -0,0 +1,38 @@
+#ifndef SingleInvertedDZ1_h
+#define SingleInvertedDZ1_h
+
+#include <ControlLoop.h>
+
+// Initial Gains
+#define B1_INIT		   	(530)
+#define	B2_INIT	  		(-130)
+#define A2_INIT			(4)
+#define R_SHIFT_INIT	(3)
+#define CONTROL_SHIFT	(4)
+
+class SingleInvertedDZ1 : public ControlLoop
+{
+  public:
+    SingleInvertedDZ1(void);
+	
+	// preSample: called during the previous sample, after control output is finished, to prepare for next sample
+    virtual void preSample();
+	
+	// postSample: called after sample. Passed sensor data. Returns control output for this sample.
+    virtual int16_t postSample(int16_t bottomAngle, int16_t topAngle);
+	
+	virtual void parseTuning( Stream* stream );
+	
+	virtual void getInfo( Stream* stream );
+	
+  private:
+	int16_t b1;
+	int16_t b2;
+	int16_t a2;
+	uint8_t rShift;
+	
+	int16_t prevBottomAngle;
+	int16_t control;
+};
+
+#endif // SingleInvertedDZ1_h
