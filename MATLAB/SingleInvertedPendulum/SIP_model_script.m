2ef4822e83c01e680eedaa42d898bf435cd21fe3
botguy@gmail.com <botguy@gmail.com@0d006a46-d63c-e3c8-9189-067751c37eed>
2013-03-21 17:29:20 +0000

    Single Inverted Pendulum Controller order reduction and tachNoise

--- MATLAB/SingleInvertedPendulum/SIP_model_script.m
+++ MATLAB/SingleInvertedPendulum/SIP_model_script.m
@@ -17,8 +17,8 @@ Ts = 1/500; %500 Hz sampling rate
 Q = eye(3);
 R = 1;
 Qn = 10*eye(3); %model is noisy since it is a linearize version of nonlinear model.
-Rn = 1; %sensors have very little noise. Only quantization noise.
-K = lqrGains(Areal, Breal, Ts, Q, R, Qn, Rn);
+Rn = eye(2); %sensors have very little noise. Only quantization noise.
+[K, est, lqg, ctlr]  = lqrGains(Areal, Breal, Ts, Q, R, Qn, Rn);
 
 % simulate
 x0 = 0;
--- MATLAB/plotData.m
+++ MATLAB/plotData.m
@@ -1,17 +1,17 @@
 function plotData( data )
-    t = data(:,1);
-    bottomPot = data(:,2);
-    topPot = data(:,3);
-    control = data(:,4);
+ %   t = data(:,1);
+    bottomPot = data(:,1);
+    topPot = data(:,2);
+    control = data(:,3);
     
     %unwrap time)
-    wrapIndx = find([1; diff(t)] <0);    
-    t(wrapIndx) = t(wrapIndx) + (2^16);
+    %wrapIndx = find([1; diff(t)] <0);    
+   % t(wrapIndx) = t(wrapIndx) + (2^16);
     
     t = (1:2:2*length(data));
     
     %plot
     figure;
-    plotyy(t, [bottomPot], t, control);
+    plotyy(t, [bottomPot], t, [topPot, control]);
 end
     
